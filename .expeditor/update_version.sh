00f4aa2433ef66f57a2ffffb0f9e2911173d82ea
Tim Smith <tsmith@chef.io>
2019-01-13 21:49:41 -0800

    Wire up this project for expedfitor
    Setup expeditor to handle writing out the changelog and releasing the gem. This prevents us from missing changelog entries and speeds up the release process.
    
    Signed-off-by: Tim Smith <tsmith@chef.io>

---
+++ .expeditor/update_version.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#
+# After a PR merge, Chef Expeditor will bump the PATCH version in the VERSION file.
+# It then executes this file to update any other files/components with that new version.
+#
+
+set -evx
+
+sed -i -r "s/INSPEC_VERSION = \".+\"/INSPEC_VERSION = \"$(cat VERSION)\"/" lib/kitchen/verifier/inspec_version.rb
+
+# Once Expeditor finshes executing this script, it will commit the changes and push
+# the commit as a new tag corresponding to the value in the VERSION file.
