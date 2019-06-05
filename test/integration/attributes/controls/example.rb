8ec43d9004634f456b0985dc94415dec6f5e317b
Clinton Wolfe <clintoncwolfe@gmail.com>
2019-06-05 13:24:11 -0400

    Rename test values for clarity
    Signed-off-by: Clinton Wolfe <clintoncwolfe@gmail.com>

--- test/integration/attributes/controls/example.rb
+++ test/integration/attributes/controls/example.rb
@@ -1,11 +1,11 @@
 # encoding: utf-8
-val_user = attribute("user", default: "alice", description: "An identification for the user")
+val_user = attribute("user", default: "value_from_dsl", description: "An identification for the user")
 val_password = attribute("password", description: "A value for the password")
 
 describe val_user do
-  it { should eq "bob" }
+  it { should eq "value_from_kitchen_yml_1" }
 end
 
 describe val_password do
-  it { should eq "secret" }
+  it { should eq "value_from_kitchen_yml_2" }
 end
