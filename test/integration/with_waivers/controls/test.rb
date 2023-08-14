dcc104883c513beea13a9613447e60cfd503c6fa
Nik08 <nikita.mathur@progress.com>
2023-08-14 12:47:22 +0530

    Linting fixes
    Signed-off-by: Nik08 <nikita.mathur@progress.com>

--- test/integration/with_waivers/controls/test.rb
+++ test/integration/with_waivers/controls/test.rb
@@ -1,12 +1,11 @@
-control 'passing-test' do
-  describe 'true' do
-    it { should cmp 'true' }
+control "passing-test" do
+  describe "true" do
+    it { should cmp "true" }
   end
 end
 
-
-control 'failing-test' do
-  describe 'false' do
-    it { should cmp 'true' }
+control "failing-test" do
+  describe "false" do
+    it { should cmp "true" }
   end
 end
